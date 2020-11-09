7c43f64d6e77b45b00efd790fdeefe2e3a5f9dd3
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-11-09 09:33:17 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,47 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+    this.state = {
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          hour12: true
+        })
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <div className="Clock">
+            <h3 id="time">{this.state.time}</h3>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
